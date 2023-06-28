f8f60a37504a286fb673c3a0769deb87ae351119
Anonymous096 <sakshamsharmaofficial7@gmail.com>
2023-06-28 19:31:21 +0530

    file io

---
+++ file_io_basics.py
@@ -0,0 +1,11 @@
+# Files IO Basics
+
+'''
+"r" = open file for reading
+"w" = open file for writing
+"x" = create file if not existed
+"a" = add more content to a file
+"t" = text mode
+"b" = binary mode
+"+" = read and write
+'''
