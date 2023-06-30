0a11055d62bb0f2429ca56c15922a0c80a897099
Anonymous096 <sakshamsharmaofficial7@gmail.com>
2023-06-30 20:06:49 +0530

    file write commit

--- file_io_basics.py
+++ file_io_basics.py
@@ -23,3 +23,8 @@ print(f.readlines()) # This will stores the lines of content of file in a list f
 
 
 f.close() # This will close the file and it is good to close a file after reading because it will free the resources which had been used to make it open and read and if any other program needs to read this file it can do it easily
+
+# Even to write in the file we have to open the file and then write it.
+d = open("file_basics.txt", "w") # Here putting writing mode <"w"> is mandetory and and the if you put the file name which already exists then it would over write it and all the stuff which was written before will be ddeleted, if you put new name of file it will create the file by itself in this case it made <"file_basics.txt">
+d.write("This is written from the python code using write function") # It will write the content in the file
+d.close() # Close the file whenever not in need
