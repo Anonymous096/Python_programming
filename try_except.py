44b00ef72e0bc537f3fa9f48b42c391e84dbf9df
Anonymous096 <sakshamsharmaofficial7@gmail.com>
2023-07-04 12:21:23 +0530

    try except basics

--- try_except.py
+++ try_except.py
@@ -4,9 +4,9 @@ try:
     c = a / b
     print(c)
 
-except ZeroDivisionError:
+except ZeroDivisionError:       # This will handle the error "ZeroDivisionError" is the type of error like "ValueError" we can use except as it is. If the try code will generate error then except statement will work
     print("Can't divide by zero")
-else:
+else:       # This will print if and only if try code will not generate error
     print("else block")
-finally:
+finally:    # This will print in every condition no matter what
     print("This will print no matter what")
